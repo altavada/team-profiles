807cc1f20a8eabd6737cd49c78824b475ba3447a
altavada <117209509+altavada@users.noreply.github.com>
2023-02-08 18:30:02 -0500

    revised parent class

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/employee');
+
+function menu() {
+    inquirer
+        .prompt([
+            // menu prompt
+        ])
+        .then((response) => {
+            switch(response.menuoption) {
+                case "Intern":
+            
+                case "Engineer":
+
+                default:
+                    console.log("Err");
+            }
+        })
+}

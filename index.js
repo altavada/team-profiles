d677e5bdd28fb0f2275ca0eaee7c5ece754f59e8
altavada <117209509+altavada@users.noreply.github.com>
2023-02-10 20:34:22 -0500

    creating inquirer prompts

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/employee');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer')
+let managerInfo;
+let engineerInfo;
+let internInfo;
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: "Enter team manager's name here:",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "Enter team manager's employee ID:",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "Enter team manager's email address:",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "Enter team manager's office number here:",
+        name: 'officeno',
+    }
+])
+.then((res) => {
+    if(res.name && res.id && res.email && res.officeno) {
+        managerInfo = new Manager(res.name, res.id, res.email, res.officeno);
+        console.log(managerInfo.name);
+        return;
+    } else {
+        console.log('Manager info incomplete.');
+        return;
+    }
+})
+
 
 function menu() {
     inquirer
